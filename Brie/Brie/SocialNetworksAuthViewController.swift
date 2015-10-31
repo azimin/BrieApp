b156ab77fc624fd4f6fa2277d9668e2073b72750
Alex Zimin <azimin@me.com>
2015-10-31 12:42:03 +0300

    merging conflict solved

--- Brie/Brie/SocialNetworksAuthViewController.swift
+++ Brie/Brie/SocialNetworksAuthViewController.swift
@@ -10,47 +10,47 @@ import UIKit
 
 class SocialNetworksAuthViewController: UIViewController {
 
-//    func VKAuth() {
-//        VKSdk.authorize([VK_PER_OFFLINE, VK_PER_WALL, VK_PER_FRIENDS], revokeAccess: true)
-//    }
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//      
-//        VKSdk.initializeWithDelegate(self, andAppId: "5128197")
-//        if VKSdk.wakeUpSession() {
-//            print("AUTHORIZED AND READY TO GO")
-//        } else {
-//            print("LET'S AUTHORIZE")
-//            
-//        }
-//    }
-//
-//}
-//
-//extension SocialNetworksAuthViewController: VKSdkDelegate {
-//    func vkSdkNeedCaptchaEnter(captchaError: VKError) {
-//        println("Need captcha")
-//    }
-//    
-//    func vkSdkTokenHasExpired(expiredToken: VKAccessToken) {
-//        println("Token has exp")
-//    }
-//    
-//    func vkSdkUserDeniedAccess(authorizationError: VKError) {
-//        println("Access denied")
-//    }
-//    
-//    func vkSdkShouldPresentViewController(controller: UIViewController) {
-//        println("Should present VC")
-//        presentViewController(controller, animated: true) { () -> Void in
-//            println("Success!")
-//        }
-//    }
-//    
-//    func vkSdkReceivedNewToken(newToken: VKAccessToken) {
-//        println("Succesfully authorized! User ID:")
-//        println(VKSdk.getAccessToken().userId)
-//        self.performSegueWithIdentifier("Are You Couch segue", sender: nil)
-//    }
+    func VKAuth() {
+        VKSdk.authorize([VK_PER_OFFLINE, VK_PER_WALL, VK_PER_FRIENDS], revokeAccess: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        VKSdk.initializeWithDelegate(self, andAppId: "5128197")
+        if VKSdk.wakeUpSession() {
+            print("AUTHORIZED AND READY TO GO")
+        } else {
+            print("LET'S AUTHORIZE")
+            
+        }
+    }
+}
+
+
+extension SocialNetworksAuthViewController: VKSdkDelegate {
+    func vkSdkNeedCaptchaEnter(captchaError: VKError) {
+        print("Need captcha")
+    }
+    
+    func vkSdkTokenHasExpired(expiredToken: VKAccessToken) {
+        print("Token has exp")
+    }
+    
+    func vkSdkUserDeniedAccess(authorizationError: VKError) {
+        print("Access denied")
+    }
+    
+    func vkSdkShouldPresentViewController(controller: UIViewController) {
+        print("Should present VC")
+        presentViewController(controller, animated: true) { () -> Void in
+            print("Success!")
+        }
+    }
+    
+    func vkSdkReceivedNewToken(newToken: VKAccessToken) {
+        print("Succesfully authorized! User ID:")
+        print(VKSdk.getAccessToken().userId)
+        self.performSegueWithIdentifier("Are You Couch segue", sender: nil)
+    }
 }
