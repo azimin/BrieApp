6b81a0d5ef8e02b4c2ab3a779a32829b22f80262
Alex Zimin <azimin@me.com>
2015-10-31 11:24:02 +0300

    base

---
+++ Brie/Brie/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Brie
+//
+//  Created by Alex Zimin on 31/10/15.
+//  Copyright © 2015 700 km. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
