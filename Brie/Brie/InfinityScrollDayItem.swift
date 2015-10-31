db953507a8f46e887faefe5cdbc5439d49b64701
Alex Zimin <azimin@me.com>
2015-10-31 18:59:36 +0300

    added infinity scroll cell

---
+++ Brie/Brie/InfinityScrollDayItem.swift
@@ -0,0 +1,13 @@
+//
+//  InfinityScrollDayItem.swift
+//  Brie
+//
+//  Created by Alex Zimin on 31/10/15.
+//  Copyright © 2015 700 km. All rights reserved.
+//
+
+import UIKit
+
+class InfinityScrollDayItem: UIView {
+  
+}
