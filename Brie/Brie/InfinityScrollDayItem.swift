ea566bb8ebaed144183c2ec989699e67d28fe126
Alex Zimin <azimin@me.com>
2015-10-31 19:15:09 +0300

    small fix

--- Brie/Brie/InfinityScrollDayItem.swift
+++ Brie/Brie/InfinityScrollDayItem.swift
@@ -9,5 +9,24 @@
 import UIKit
 
 class InfinityScrollDayItem: UIView {
+  let dateLabel = UILabel()
+  let monthLabel = UILabel()
   
+  init() {
+    super.init(frame: CGRect(x: 0, y: 0, width: 75, height: 75))
+    
+    dateLabel.frame = CGRect(x: 16, y: 17, width: 42, height: 21)
+    dateLabel.text = "31"
+    dateLabel.textAlignment = .Center
+    self.addSubview(dateLabel)
+    
+    monthLabel.frame = CGRect(x: 16, y: 17, width: 42, height: 21)
+    monthLabel.text = "Nov"
+    monthLabel.textAlignment = .Center
+    self.addSubview(monthLabel)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 }
