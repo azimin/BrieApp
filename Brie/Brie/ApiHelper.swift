3361d60d41bd47c61d2d2b78b63ee3d9b5b90f3e
Vadim Drobinin <vadim@drobinin.com>
2015-10-31 11:37:27 +0300

    Add templates for API auth

---
+++ Brie/Brie/ApiHelper.swift
@@ -0,0 +1,25 @@
+//
+//  ApiHelper.swift
+//  Brie
+//
+//  Created by Vadim Drobinin on 31/10/15.
+//  Copyright © 2015 700 km. All rights reserved.
+//
+
+import Foundation
+
+class VKAuth {
+    
+}
+
+class UberAuth {
+    
+}
+
+class KudaGoAuth {
+    
+}
+
+class IikoAuth {
+    
+}
---
+++ Brie/Brie/SocialNetworksAuthViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SocialNetworksAuthViewController.swift
+//  Brie
+//
+//  Created by Vadim Drobinin on 31/10/15.
+//  Copyright © 2015 700 km. All rights reserved.
+//
+
+import UIKit
+
+class SocialNetworksAuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        VKSdk.initializeWithDelegate(self, andAppId: "5128197")
+        if VKSdk.wakeUpSession() {
+            print("")
+        }
+    }
+
+}
